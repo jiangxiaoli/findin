4ab3f3a95aa536f3f34447f27c6c833527938656
Xiaoli Jiang <jiangxiaoli1104@gmail.com>
2015-07-16 22:47:38 -0700

    pictureUrl

--- schemas/invitation_schemas.py
+++ schemas/invitation_schemas.py
@@ -8,8 +8,8 @@ class InvitationSchema(Schema):
     status = fields.Str()
     createTime = fields.DateTime(dump_only=True, attribute='create_time')
     updateTime = fields.DateTime(dump_only=True, attribute='update_time')
-    inviter = fields.Nested(UserSchema, only=["id", "firstName", "lastName", "headline", "public_profile_url"])
-    invitee = fields.Nested(UserSchema, only=["id", "firstName", "lastName", "headline", "public_profile_url"])
+    inviter = fields.Nested(UserSchema, only=["id", "firstName", "lastName", "headline", "pictureUrl"])
+    invitee = fields.Nested(UserSchema, only=["id", "firstName", "lastName", "headline", "pictureUrl"])
 
 invitation_schema = InvitationSchema()
 invitations_schema = InvitationSchema(many=True)
